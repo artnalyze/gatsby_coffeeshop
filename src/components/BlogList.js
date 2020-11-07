d1ed097ac8f70e25621f78f226735747b77a84f1
Artnalyze <artnalyze@gmail.com>
2020-11-07 09:02:59 +0700

    update Linking to the dynamically generated pages

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import BlogPost from './BlogPost'
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import BlogPost from "./BlogPost"
 
 export default function BlogList() {
-    const data = useStaticQuery(graphql`
-        {
-            allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC}) {
-                edges {
-                    node {
-                        id
-                        frontmatter {
-                            title
-                            date(formatString: "MMMM D, YYYY")
-                        }
-                        excerpt
-                    }
-                }
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date(formatString: "MMMM D, YYYY")
             }
+            fields {
+              slug
+            }
+            excerpt
+          }
         }
-    `);
-    return (
-        <div>
-            {data.allMarkdownRemark.edges.map(edges => (
-                <BlogPost
-                    key={edges.node.id}
-                    title={edges.node.frontmatter.title}
-                    date={edges.node.frontmatter.date}
-                    excerpt={edges.node.excerpt}
-                ></BlogPost>   
-            ))}
-        </div>
-    )
+      }
+    }
+  `)
+  return (
+    <div>
+      {data.allMarkdownRemark.edges.map(edges => (
+        <BlogPost
+          key={edges.node.id}
+          slug={edges.node.fields.slug}
+          title={edges.node.frontmatter.title}
+          date={edges.node.frontmatter.date}
+          excerpt={edges.node.excerpt}
+        ></BlogPost>
+      ))}
+    </div>
+  )
 }
