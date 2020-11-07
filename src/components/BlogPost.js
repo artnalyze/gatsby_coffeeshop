3676fa20b4c93f72209943b22b900aba43fafc22
Artnalyze <artnalyze@gmail.com>
2020-11-07 10:12:20 +0700

    Edit BlogPost.js

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,7 +5,7 @@ import styles from "./BlogPost.module.css"
 export default function BlogPost({ title, date, excerpt, slug }) {
   return (
     <article className={styles.blog}>
-          <h2><Link to={slug}>{title}</Link></h2>
+      <h2><Link to={slug}>{title}</Link></h2>
       <h3>{date}</h3>
       <p>{excerpt}</p>
     </article>
